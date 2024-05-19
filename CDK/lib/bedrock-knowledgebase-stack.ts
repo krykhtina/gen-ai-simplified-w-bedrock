dfa63b4e44bb00e6b6cf06f9c7cc4781883a47f8
anadabrowska <anadabrowska98@gmail.com>
2024-05-19 21:55:27 +0200

    Knowladgebase stack pt.2

--- CDK/lib/bedrock-knowledgebase-stack.ts
+++ CDK/lib/bedrock-knowledgebase-stack.ts
@@ -19,7 +19,12 @@ import {
   CfnCollection,
   CfnSecurityPolicy,
 } from "aws-cdk-lib/aws-opensearchserverless";
-import { Architecture, Runtime } from "aws-cdk-lib/aws-lambda";
+import {
+  Architecture,
+  Code,
+  LayerVersion,
+  Runtime,
+} from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Provider } from "aws-cdk-lib/custom-resources";
 import { RetentionDays } from "aws-cdk-lib/aws-logs";
@@ -117,81 +122,89 @@ export class BedrockKnowledgeBaseStack extends Stack {
       },
     });
 
+    const dependencyLayer = new LayerVersion(this, "dependencies-layer", {
+      code: Code.fromAsset("./lib/knowledgeBase/dependencies"),
+      description: "dependencies for lambda",
+      compatibleArchitectures: [Architecture.X86_64],
+      compatibleRuntimes: [Runtime.NODEJS_18_X],
+    });
+
     const knowledgeBaseCustomResource = new NodejsFunction(
       this,
       "knowledgeBaseCustomResourceLambda",
       {
         entry: "./lib/knowledgeBase/indexCreator.ts",
         handler: "handler",
-        runtime: Runtime.NODEJS_16_X,
+        runtime: Runtime.NODEJS_18_X,
         timeout: Duration.minutes(15),
         role: knowledgeBaseCustomResourceRole,
+        layers: [dependencyLayer],
       }
     );
 
-    // const knowledgeBaseProvider = new Provider(this, "knowledgeBaseProvider", {
-    //   onEventHandler: knowledgeBaseCustomResource,
-    //   logRetention: RetentionDays.ONE_WEEK,
+    const knowledgeBaseProvider = new Provider(this, "knowledgeBaseProvider", {
+      onEventHandler: knowledgeBaseCustomResource,
+      logRetention: RetentionDays.ONE_WEEK,
+    });
+
+    const bedrockKnowledgeBase = new CustomResource(
+      this,
+      "KnowledgeBaseCustomResource",
+      {
+        serviceToken: knowledgeBaseProvider.serviceToken,
+        properties: {
+          namePrefix: "bedrock-knowledge-base",
+          collectionEndpoint: openSearchServerless.attrCollectionEndpoint,
+        },
+      }
+    );
+
+    // const knowledgeBase = new CfnKnowledgeBase(this, "BedrockKnowledgebase", {
+    //   name: KNOWLEDGE_BASE_NAME,
+    //   description:
+    //     "This knowladge base contains knowladge about the properties available in OMG booking details like emergency procedures, security, rules, utilities, decor, location, local area description and others",
+    //   roleArn: bedrockExecutionRole.roleArn,
+    //   knowledgeBaseConfiguration: {
+    //     type: "VECTOR",
+    //     vectorKnowledgeBaseConfiguration: {
+    //       embeddingModelArn: embeddingModelArn,
+    //     },
+    //   },
+    //   storageConfiguration: {
+    //     type: "OPENSEARCH_SERVERLESS",
+    //     opensearchServerlessConfiguration: {
+    //       collectionArn: openSearchServerless.attrArn,
+    //       vectorIndexName: VECTOR_STORE_NAME,
+    //       fieldMapping: {
+    //         vectorField: "vector",
+    //         textField: "text",
+    //         metadataField: "text-metadata",
+    //       },
+    //     },
+    //   },
     // });
 
-    // const bedrockKnowledgeBase = new CustomResource(
-    //   this,
-    //   "KnowledgeBaseCustomResource",
-    //   {
-    //     serviceToken: knowledgeBaseProvider.serviceToken,
-    //     properties: {
-    //       namePrefix: "bedrock-knowledge-base",
-    //       collectionEndpoint: openSearchServerless.attrCollectionEndpoint,
+    // const dataSource = new CfnDataSource(this, "DataSource", {
+    //   name: KNOWLEDGE_BASE_NAME,
+    //   description: "Knowledgebase s3 datasource",
+    //   knowledgeBaseId: knowledgeBase.attrKnowledgeBaseId,
+    //   dataSourceConfiguration: {
+    //     type: "S3",
+    //     s3Configuration: {
+    //       // TODO: create bucket here
+    //       bucketArn: KB_BUCKET_ARN,
     //     },
-    //   }
-    // );
-
-    const knowledgeBase = new CfnKnowledgeBase(this, "BedrockKnowledgebase", {
-      name: KNOWLEDGE_BASE_NAME,
-      description:
-        "This knowladge base contains knowladge about the properties available in OMG booking details like emergency procedures, security, rules, utilities, decor, location, local area description and others",
-      roleArn: bedrockExecutionRole.roleArn,
-      knowledgeBaseConfiguration: {
-        type: "VECTOR",
-        vectorKnowledgeBaseConfiguration: {
-          embeddingModelArn: embeddingModelArn,
-        },
-      },
-      storageConfiguration: {
-        type: "OPENSEARCH_SERVERLESS",
-        opensearchServerlessConfiguration: {
-          collectionArn: openSearchServerless.attrArn,
-          vectorIndexName: VECTOR_STORE_NAME,
-          fieldMapping: {
-            vectorField: "vector",
-            textField: "text",
-            metadataField: "text-metadata",
-          },
-        },
-      },
-    });
-
-    const dataSource = new CfnDataSource(this, "DataSource", {
-      name: KNOWLEDGE_BASE_NAME,
-      description: "Knowledgebase s3 datasource",
-      knowledgeBaseId: knowledgeBase.attrKnowledgeBaseId,
-      dataSourceConfiguration: {
-        type: "S3",
-        s3Configuration: {
-          // TODO: create bucket here
-          bucketArn: KB_BUCKET_ARN,
-        },
-      },
-      vectorIngestionConfiguration: {
-        chunkingConfiguration: {
-          chunkingStrategy: "NONE",
-          // fixedSizeChunkingConfiguration: {
-          //   maxTokens: 512,
-          //   overlapPercentage: 20,
-          // },
-        },
-      },
-    });
+    //   },
+    //   vectorIngestionConfiguration: {
+    //     chunkingConfiguration: {
+    //       chunkingStrategy: "NONE",
+    //       // fixedSizeChunkingConfiguration: {
+    //       //   maxTokens: 512,
+    //       //   overlapPercentage: 20,
+    //       // },
+    //     },
+    //   },
+    // });
 
     // new CfnOutput(this, 'KnowledgeBaseId', { value: this.knowledgeBase.knowledgeBaseId });
     // new CfnOutput(this, 'DataSourceId', { value: this.dataSource.dataSourceId });
--- CDK/lib/knowledgeBase/openSearchClient.ts
+++ CDK/lib/knowledgeBase/openSearchClient.ts
@@ -1,6 +1,11 @@
-import { Client } from "@opensearch-project/opensearch/.";
+import { Client } from "@opensearch-project/opensearch";
 import { AwsSigv4Signer } from "@opensearch-project/opensearch/aws";
 import { defaultProvider } from "@aws-sdk/credential-provider-node";
+import {
+  CreateAccessPolicyCommand,
+  OpenSearchServerlessClient,
+} from "@aws-sdk/client-opensearchserverless";
+import { randomUUID } from "crypto";
 
 const AWS_REGION = "us-west-2";
 
@@ -57,4 +62,79 @@ export const createIndex = async (params: CreateIndexParams) => {
   } catch (error) {
     console.error(JSON.stringify(error, null, 2));
   }
+};
+
+interface CreateAccessPolicyParams {
+  namePrefix: string;
+  knowledgeBaseRoleArn: string;
+  knowledgeBaseCustomResourceRole: string;
+  accessPolicyArns: string;
+}
+
+export const createAccessPolicy = async (params: CreateAccessPolicyParams) => {
+  console.log("Creating AccessPolicy");
+  const {
+    namePrefix,
+    knowledgeBaseRoleArn,
+    knowledgeBaseCustomResourceRole,
+    accessPolicyArns,
+  } = params;
+
+  const parsedArns: string[] = JSON.parse(accessPolicyArns);
+  const principalArray = [
+    ...parsedArns,
+    knowledgeBaseRoleArn,
+    knowledgeBaseCustomResourceRole,
+  ];
+
+  const policy = [
+    {
+      Rules: [
+        {
+          Resource: [`collection/${namePrefix}`],
+          Permission: [
+            "aoss:DescribeCollectionItems",
+            "aoss:CreateCollectionItems",
+            "aoss:UpdateCollectionItems",
+          ],
+          ResourceType: "collection",
+        },
+        {
+          Resource: [`index/${namePrefix}-/*`],
+          Permission: [
+            "aoss:UpdateIndex",
+            "aoss:DescribeIndex",
+            "aoss:ReadDocument",
+            "aoss:WriteDocument",
+            "aoss:CreateIndex",
+          ],
+          ResourceType: "index",
+        },
+      ],
+      Principal: principalArray,
+      Description: "",
+    },
+  ];
+
+  console.log(`Access Policy: ${JSON.stringify(policy, null, 2)}`);
+  try {
+    const openSearchServerlessClient = new OpenSearchServerlessClient({
+      region: AWS_REGION,
+    });
+
+    const data = await openSearchServerlessClient.send(
+      new CreateAccessPolicyCommand({
+        clientToken: randomUUID(),
+        name: `${namePrefix}`,
+        type: "data",
+        policy: JSON.stringify(policy),
+      })
+    );
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error);
+    }
+  }
+  throw new Error("Failed to create AccessPolicy");
 };
