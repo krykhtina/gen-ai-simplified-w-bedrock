b315b718ea94a81c5427ffee78187a5f40666062
Mikołaj Olszewski <mikolo@amazon.com>
2024-05-29 13:23:59 +0200

    Make skeleton deployable

--- BookingAPI/cmd/functions/search/search.go
+++ BookingAPI/cmd/functions/search/search.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -12,7 +13,20 @@ type Request = events.APIGatewayProxyRequest
 type Response = events.APIGatewayProxyResponse
 
 func handler(request Request) (Response, error) {
-	result := []string{}
+	params := request.QueryStringParameters
+	if params == nil {
+		return Response{
+			Body:       "No query parameters found",
+			StatusCode: 400,
+		}, nil
+	}
+	result := []string{
+		fmt.Sprintf("location: %s", params["location"]),
+		fmt.Sprintf("number_of_beds: %s", params["number_of_beds"]),
+		fmt.Sprintf("accessibility: %s", params["accessibility"]),
+		fmt.Sprintf("pet_friendly: %s", params["pet_friendly"]),
+		fmt.Sprintf("food_accommodation: %s", params["food_accommodation"]),
+	}
 	body, err := json.Marshal(result)
 	if err != nil {
 		return Response{}, err
